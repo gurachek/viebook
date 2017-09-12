1cc22748ad1ae9813cbd7aa0e2769bbd250a5d76
GallantGentleman <webcrash@mail.ua>
2017-09-12 21:55:08 +0300

    Liveinternet counter

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -179,7 +179,20 @@ AppAsset::register($this);
 <footer class="footer">
     <div class="container">
         <p class="pull-left">&copy; Viebook <?= date('Y') ?></p>
-        <p class="pull-right">Gurachek</p>
+        <p class="pull-right">
+
+            <!--LiveInternet counter--><script type="text/javascript">
+            document.write("<a href='//www.liveinternet.ru/click' "+
+            "target=_blank><img src='//counter.yadro.ru/hit?t44.6;r"+
+            escape(document.referrer)+((typeof(screen)=="undefined")?"":
+            ";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+            screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+            ";"+Math.random()+
+            "' alt='' title='LiveInternet' "+
+            "border='0' width='31' height='31'><\/a>")
+            </script><!--/LiveInternet-->
+
+        </p>
         <p class="text-center">Читайте <a href="http://blog.viebook.ru/" target="_blank">блог</a> проекта.
         </p>
     </div>
@@ -187,6 +200,18 @@ AppAsset::register($this);
 
 <footer class="mobile-footer">
     <p class="text-center">Хотите следить за развитием пректа? Посетите наш <a href="http://blog.viebook.ru" target="_blank">блог</a></p>
+    <p class="text-right">
+        <!--LiveInternet counter--><script type="text/javascript">
+            document.write("<a href='//www.liveinternet.ru/click' "+
+            "target=_blank><img src='//counter.yadro.ru/hit?t44.6;r"+
+            escape(document.referrer)+((typeof(screen)=="undefined")?"":
+            ";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+            screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+            ";"+Math.random()+
+            "' alt='' title='LiveInternet' "+
+            "border='0' width='31' height='31'><\/a>")
+            </script><!--/LiveInternet-->
+    </p>
 </div>
 
 <?php $this->endBody() ?>
