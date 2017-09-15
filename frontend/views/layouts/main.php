8536c28cc5b9741187c8893a96d861085dce50f0
GallantGentleman <webcrash@mail.ua>
2017-09-15 23:14:24 +0300

    Add yandex meta tag

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -58,6 +58,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="yandex-verification" content="07e19644d4902619" />
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
