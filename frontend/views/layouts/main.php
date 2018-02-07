e0472a8b1d9ed97840ebe0db6a2eb7455cd17f4f
gallantgentleman <webcrash091@gmail.com>
2018-02-07 19:49:31 +0200

    Google verification code

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -77,7 +77,7 @@ AppAsset::register($this);
 
 <?php endif; ?>
 
-    <meta name="google-site-verification" content="kjR3xvzotZkHnl8Y6NC4iF0XF_PUGrd8UMwLziGEenk" />
+    <meta name="google-site-verification" content="Rk-U-fzLha7dGP0kyRnzatleu8xBBfBP0xcYSG6Lajs" />
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="yandex-verification" content="07e19644d4902619" />
