8f5cb2f99059842158907e93de736d575eee419e
gallantgentleman <webcrash091@gmail.com>
2018-02-07 10:42:02 +0200

    some issues

--- frontend/views/book/list.php
+++ frontend/views/book/list.php
@@ -35,7 +35,7 @@ $this->registerJS($js);
 
 		<?php foreach($books as $book): ?>
 
-			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-10 col-xs-offset-1 book-list-col">
+			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 book-list-col">
 				
 				<div class="book-list">
 					<div class="short-info text-center">
